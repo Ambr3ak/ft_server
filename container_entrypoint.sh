b97d70b39ca7b50ab27ef64792a5785a81a0640a
Ambre BOURDARIE <abourdar@e2r2p8.42.fr>
2021-02-17 15:51:29 +0100

    t

--- container_entrypoint.sh
+++ container_entrypoint.sh
@@ -7,9 +7,11 @@ cp nginx.conf /etc/nginx/sites-enabled/
 
 service nginx start
 service nginx status
-cd /etc/nginx/
-mkdir ssl
-cd ssl/
-openssl req –new –newkey rsa:2048 –nodes –keyout localhost.key –out localhost.csr
+cp nginx.conf nginx.conf.bak
+mkdir /etc/nginx/ssl
+chmod 700 /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=ambre/CN=localhost"
+nginx restart
+
 
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log
